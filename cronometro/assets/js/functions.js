6484bb99338d1c495e8f1298ad606759d658f4b4
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-10 15:43:29 +0200

    feat: update formatTime function

--- cronometro/assets/js/functions.js
+++ cronometro/assets/js/functions.js
@@ -1,28 +1,20 @@
 function stopTime() {
-    flag = true;
+  flag = true;
 }
 
 function resetTime() {
-
   flag = true;
   seconds = 0;
   minutes = 0;
 
   formatTime();
-
 }
 
-
-function formatTime (){
-    if (minutes < 10) {
-        minuteElem.innerHTML = "0" + minutes;
-      } else {
-        minuteElem.innerHTML = minutes;
-      }
-
-      if (seconds < 10) {
-        secondElem.innerHTML = "0" + seconds;
-      } else {
-        secondElem.innerHTML = seconds;
-      }
+function formatTime() {
+  minutes < 10
+    ? (minuteElem.innerHTML = "0" + minutes)
+    : (minuteElem.innerHTML = minutes);
+  seconds < 10
+    ? (secondElem.innerHTML = "0" + seconds)
+    : (secondElem.innerHTML = seconds);
 }

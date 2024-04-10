8653ff2073c818a51ef7a0916518305bafdd7668
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-04-10 13:48:57 +0200

    feat: ui added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-let sec = 40;
+let sec = 48;
+let maxSec=60;
+const printResult = document.getElementById("print");
 
-const countdown = setInterval(function(){
-    if(sec > 49) {
-        console.log(sec-60);
-        if(sec === 60){
-            console.log("BUON ANNO");
-            clearInterval(countdown);
-        }
+const countdown = setInterval(function () {
+//   printResult.innerHTML = sec;
+
+  if (sec > 49) {
+    printResult.innerHTML = maxSec - sec;
+    if (sec === maxSec) {
+      printResult.innerHTML = `<img src="../img/meme.webp">`;
+
+      clearInterval(countdown);
     }
+  }
 
-    console.log(sec);
-    sec++;
+  console.log(sec);
+  sec++;
 }, 1000);
-
